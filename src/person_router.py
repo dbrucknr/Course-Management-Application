c79b424b34084b716a371effb106bba477e3f923
Derek Bruckner <dbrucknr@umich.edu>
2024-07-13 16:21:36 -0400

    Turn repeated code into re-usable dependency that's injected.

--- src/person_router.py
+++ src/person_router.py
@@ -1,83 +1,51 @@
 from math import ceil
+from typing import AsyncGenerator, Type
 from fastapi import APIRouter, Depends, Query
-from sqlmodel import select, func
+from sqlmodel import select, func, text
 from sqlmodel.ext.asyncio.session import AsyncSession
 from main import database
 from models import *
 from schemas import PaginatedResponse
+from functools import partial
 
 persons_router = APIRouter(prefix="/people", tags=["People"])
 
+
+async def count_records(model: Type[SQLModel], session: AsyncSession,) -> AsyncGenerator[int, None]:
+    count_statement = select(func.count()).select_from(model)
+    count_result = await session.exec(count_statement)
+    total_count = count_result.one()
+    return total_count
+
+async def select_all_paginated(
+    model: Type[SQLModel], 
+    offset: int = 1,
+    limit: int = Query(default=100, le=100),
+    session: AsyncSession = Depends(database.get_session),
+) -> AsyncGenerator[PaginatedResponse, None]:
+    
+    record_count = await count_records(model, session)
+    skip = (offset - 1) * limit
+    statement = select(model).offset(skip).limit(limit)
+    result = await session.exec(statement)
+
+    yield PaginatedResponse(
+        current_page=offset,
+        total_pages=ceil(record_count / limit),
+        count=record_count, 
+        data=result.all()
+    )
+
 # GET: http://localhost:8000/people/?offset=0&limit=100
 # http://localhost:8000/people/?offset=30&limit=100
-@persons_router.get("/", response_model=PaginatedResponse[PersonPublic])
-async def people(
-    session: AsyncSession = Depends(database.get_session),
-    offset: int = 0,
-    limit: int = Query(default=100, le=100)
-):
-    skip = (offset - 1) * limit
+@persons_router.get(path="/", response_model=PaginatedResponse[PersonPublic])
+async def people(records: PaginatedResponse[Person] = Depends(partial(select_all_paginated, Person))):
+    return records
 
-    count_statement = select(func.count(Person.id))
-    count_result = await session.exec(count_statement)
-    total_count = count_result.one()
-
-    statement = select(Person).offset(skip).limit(limit)
-    result = await session.exec(statement)
-    people = result.all()
-
-    return PaginatedResponse(
-        current_page=offset,
-        total_pages=ceil(total_count / limit),
-        count=total_count, 
-        data=people
-    )
-
-@persons_router.get(
-    "/students", 
-    response_model=PaginatedResponse[PublicStudent]
-)
-async def students(
-    session: AsyncSession = Depends(database.get_session),
-    offset: int = 0, 
-    limit: int = Query(default=100, le=100)
-):
-    skip = (offset - 1) * limit
-
-    count_statement = select(func.count(Student.id))
-    count_result = await session.exec(count_statement)
-    total_count = count_result.one()
-
-    statement = select(Student).offset(skip).limit(limit)
-    result = await session.exec(statement)
-    students = result.all()
-
-    return PaginatedResponse(
-        current_page=offset,
-        total_pages=ceil(total_count / limit),
-        count=total_count, 
-        data=students
-    )
+@persons_router.get(path="/students", response_model=PaginatedResponse[PublicStudent])
+async def students(records: PaginatedResponse[Student] = Depends(partial(select_all_paginated, Student))):
+    return records
 
 @persons_router.get("/instructors", response_model=PaginatedResponse[PublicInstructor])
-async def instructors(
-    session: AsyncSession = Depends(database.get_session),
-    offset: int = 0, 
-    limit: int = Query(default=100, le=100)
-):
-    skip = (offset - 1) * limit
-
-    count_statement = select(func.count(Instructor.id))
-    count_result = await session.exec(count_statement)
-    total_count = count_result.one()
-
-    statement = select(Instructor).offset(skip).limit(limit)
-    result = await session.exec(statement)
-    instructors = result.all()
-
-    return PaginatedResponse(
-        current_page=offset,
-        total_pages=ceil(total_count / limit),
-        count=total_count, 
-        data=instructors
-    )
+async def instructors(records: PaginatedResponse[Instructor] = Depends(partial(select_all_paginated, Instructor))):
+    return records
