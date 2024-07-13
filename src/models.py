5318cceda8f855301749792b793ef1a37c5ace67
Derek Bruckner <dbrucknr@umich.edu>
2024-07-13 15:35:21 -0400

    Standardize pagination across the three roles.

--- src/models.py
+++ src/models.py
@@ -26,7 +26,7 @@ from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
 
 # https://stackoverflow.com/questions/74252768/missinggreenlet-greenlet-spawn-has-not-been-called
-
+# -----------------------------------------------------------------------------------------------------------------------
 class PersonBase(SQLModel):
     name: str
     email: str = Field(index=True, unique=True)
@@ -41,25 +41,33 @@ class PersonPublic(SQLModel):
     name: str
     email: str
 
+# -----------------------------------------------------------------------------------------------------------------------
 class StudentBase(SQLModel):
-    person_id: int = Field(foreign_key="person.id")
+    pass
     
 
 class Student(StudentBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    person_id: int = Field(foreign_key="person.id")
     person: Optional[Person] = Relationship(back_populates="student", sa_relationship_kwargs={'lazy': 'selectin'})
 
 class PublicStudent(StudentBase):
     id: int
     person: PersonPublic
+# -----------------------------------------------------------------------------------------------------------------------
 
+class InstructorBase(SQLModel):
+    pass
 
-
-
-class Instructor(SQLModel, table=True):
+class Instructor(InstructorBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     person_id: int = Field(foreign_key="person.id")
-    person: Optional[Person] = Relationship(back_populates="instructor")
+    person: Optional[Person] = Relationship(back_populates="instructor", sa_relationship_kwargs={'lazy': 'selectin'})
+
+
+class PublicInstructor(InstructorBase):
+    id: int
+    person: PersonPublic
 
 # class Admin(SQLModel, table=True):
 #     id: Optional[int] = Field(default=None, primary_key=True)
--- src/person_router.py
+++ src/person_router.py
@@ -35,25 +35,49 @@ async def people(
 
 @persons_router.get(
     "/students", 
-    response_model=list[PublicStudent]
+    response_model=PaginatedResponse[PublicStudent]
 )
 async def students(
     session: AsyncSession = Depends(database.get_session),
     offset: int = 0, 
     limit: int = Query(default=100, le=100)
 ):
-    statement = select(Student).offset(offset).limit(limit)
+    skip = (offset - 1) * limit
+
+    count_statement = select(func.count(Student.id))
+    count_result = await session.exec(count_statement)
+    total_count = count_result.one()
+
+    statement = select(Student).offset(skip).limit(limit)
     result = await session.exec(statement)
     students = result.all()
-    return students
 
-@persons_router.get("/instructors")
+    return PaginatedResponse(
+        current_page=offset,
+        total_pages=ceil(total_count / limit),
+        count=total_count, 
+        data=students
+    )
+
+@persons_router.get("/instructors", response_model=PaginatedResponse[PublicInstructor])
 async def instructors(
     session: AsyncSession = Depends(database.get_session),
     offset: int = 0, 
     limit: int = Query(default=100, le=100)
 ):
-    statement = select(Instructor).offset(offset).limit(limit)
+    skip = (offset - 1) * limit
+
+    count_statement = select(func.count(Instructor.id))
+    count_result = await session.exec(count_statement)
+    total_count = count_result.one()
+
+    statement = select(Instructor).offset(skip).limit(limit)
     result = await session.exec(statement)
     instructors = result.all()
-    return instructors
+
+    return PaginatedResponse(
+        current_page=offset,
+        total_pages=ceil(total_count / limit),
+        count=total_count, 
+        data=instructors
+    )
